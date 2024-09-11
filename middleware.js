ab4c9794cb12496f7b4f061f4604a36039b6e186
dhiraj <sasmaldhiraj@gmail.com>
2024-09-11 22:19:16 +0530

    change in isloogdin middleware

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ const ExpressError= require("./utils/ExpressError.js");
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl=req.originalUrl;
-        req.flash("error","login to add property");
+        req.flash("error","you must be logged in");
        return res.redirect("/login");
     }
     next();
